59eb302f9f427213cad8546c68c4e315b20b0b47
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 19:43:03 +0530

    Update helper-hardhat-config.ts

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,3 +1,6 @@
+// Pricefeed addresses can be obtained at https://docs.chain.link/docs/reference-contracts
+// Default one is ETH / USD contract on Kovan
+
 export interface networkConfigItem {
     ethUsdPriceFeed?: string
     blockConfirmations?: number
@@ -10,8 +13,6 @@ export interface networkConfigItem {
   export const networkConfig: networkConfigInfo = {
     localhost: {},
     hardhat: {},
-    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
-    // Default one is ETH/USD contract on Kovan
     kovan: {
       blockConfirmations: 6,
     },
@@ -22,10 +23,11 @@ export interface networkConfigItem {
   
   // Governor Values
   export const QUORUM_PERCENTAGE = 4 // Need 4% of voters to pass
-  export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can enact
-  // export const VOTING_PERIOD = 45818 // 1 week - how long the vote lasts. This is pretty long even for local tests
-  export const VOTING_PERIOD = 5 // blocks
-  export const VOTING_DELAY = 1 // 1 Block - How many blocks till a proposal vote becomes active
+  export const MIN_DELAY = 3600 // 1 hour -- after a vote passes, you have 1 hour before you can enact
+  
+  // export const VOTING_PERIOD = 45818 -- 1 week -- how long the vote lasts. This is pretty long even for local tests
+  export const VOTING_PERIOD = 5 // Blocks
+  export const VOTING_DELAY = 1 // 1 Block -- How many blocks till a proposal vote becomes active
   export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
   
   export const NEW_STORE_VALUE = 77
