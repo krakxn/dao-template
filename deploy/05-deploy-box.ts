b99e6e4a89c929de538dddd95766458a8c18aff3
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 19:16:56 +0530

    Update 05-deploy-box.ts

--- deploy/05-deploy-box.ts
+++ deploy/05-deploy-box.ts
@@ -11,6 +11,7 @@ const deployBox: DeployFunction = async function (hre: HardhatRuntimeEnvironment
   const { deployer } = await getNamedAccounts()
   log("----------------------------------------------------")
   log("Deploying Box and waiting for confirmations...")
+  
   const box = await deploy("Box", {
     from: deployer,
     args: [],
@@ -18,10 +19,13 @@ const deployBox: DeployFunction = async function (hre: HardhatRuntimeEnvironment
     // we need to wait if on a live network so we can verify properly
     waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
   })
+  
   log(`Box at ${box.address}`)
+  
   if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
     await verify(box.address, [])
   }
+  
   const boxContract = await ethers.getContractAt("Box", box.address)
   const timeLock = await ethers.getContract("TimeLock")
   const transferTx = await boxContract.transferOwnership(timeLock.address)
