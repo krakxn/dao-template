48fcce427e5cde9e50caab53fca3200a4680f717
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 19:15:55 +0530

    Update 03-deploy-governor-contract.ts

--- deploy/03-deploy-governor-contract.ts
+++ deploy/03-deploy-governor-contract.ts
@@ -19,6 +19,7 @@ const deployGovernorContract: DeployFunction = async function (hre: HardhatRunti
 
   log("----------------------------------------------------")
   log("Deploying GovernorContract and waiting for confirmations...")
+  
   const governorContract = await deploy("GovernorContract", {
     from: deployer,
     args: [
@@ -32,7 +33,9 @@ const deployGovernorContract: DeployFunction = async function (hre: HardhatRunti
     // we need to wait if on a live network so we can verify properly
     waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
   })
+  
   log(`GovernorContract at ${governorContract.address}`)
+  
   if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
     await verify(governorContract.address, [])
   }
