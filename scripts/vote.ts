787acb92664340bc6b1d610644f93d41fa064180
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 19:35:52 +0530

    Update vote.ts

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -1,3 +1,5 @@
+// Note: 0 = Against, 1 = For, 2 = Abstain for this example
+
 import * as fs from "fs"
 import { network, ethers } from "hardhat"
 import { proposalsFile, developmentChains, VOTING_PERIOD } from "../helper-hardhat-config"
@@ -7,15 +9,15 @@ const index = 0
 
 async function main(proposalIndex: number) {
   const proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"))
+  
   // You could swap this out for the ID you want to use too
   const proposalId = proposals[network.config.chainId!][proposalIndex]
-  // 0 = Against, 1 = For, 2 = Abstain for this example
+ 
   const voteWay = 1
   const reason = "I lika do da cha cha"
   await vote(proposalId, voteWay, reason)
 }
 
-// 0 = Against, 1 = For, 2 = Abstain for this example
 export async function vote(proposalId: string, voteWay: number, reason: string) {
   console.log("Voting...")
   const governor = await ethers.getContract("GovernorContract")
@@ -24,6 +26,7 @@ export async function vote(proposalId: string, voteWay: number, reason: string) 
   console.log(voteTxReceipt.events[0].args.reason)
   const proposalState = await governor.state(proposalId)
   console.log(`Current Proposal State: ${proposalState}`)
+  
   if (developmentChains.includes(network.name)) {
     await moveBlocks(VOTING_PERIOD + 1)
   }
