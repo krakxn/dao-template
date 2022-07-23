9e8bb32a781c94126e7a070ab0c7ae31fad273c0
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 19:12:55 +0530

    Update Box.sol

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,4 +1,3 @@
-// contracts/Box.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -7,16 +6,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Box is Ownable {
   uint256 private value;
 
-  // Emitted when the stored value changes
+  /// Emitted when the stored value changes
   event ValueChanged(uint256 newValue);
 
-  // Stores a new value in the contract
+  /// Stores a new value in the contract
   function store(uint256 newValue) public onlyOwner {
     value = newValue;
     emit ValueChanged(newValue);
   }
 
-  // Reads the last stored value
+  /// Reads the last stored value
   function retrieve() public view returns (uint256) {
     return value;
   }
